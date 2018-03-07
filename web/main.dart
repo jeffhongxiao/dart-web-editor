8b31257e6cd31d1d32c725638aa8cc32d0820e9a
hongxiao <hongtao.xiao@unb.ca>
2018-03-07 16:13:00 -0400

    naive version of Editor

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,28 @@
 import 'dart:html';
+import 'dart:convert' show JSON;
+
+TextAreaElement theEditor;
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  theEditor = querySelector('#editor');
+  theEditor.onKeyUp.listen(handleKeyPress);
+  theEditor.text = loadDocument();
+  print("[DEBUG] editor's text has length = " + theEditor.text.length.toString());
+}
+
+void handleKeyPress(KeyboardEvent event) {
+  saveDocument();
+}
+
+String loadDocument() {
+  String readings = "";
+  String jsonString = window.localStorage["MyTextEditor"];
+  if (jsonString != null && jsonString.length > 0) {
+    readings = JSON.decode(jsonString);
+  }
+  return readings;
+}
+
+void saveDocument() {
+  window.localStorage["MyTextEditor"] = JSON.encode(theEditor.value);
 }
